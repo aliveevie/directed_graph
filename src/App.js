d9b217ab3e0487229f0f7b42c34a088bcf793c15
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2023-08-16 17:42:34 +0100

    modified files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import './App.css'
 
 function App() {
   const containerRef = useRef(null);
+  const [nodes, setNodes] = useState([]);
+  const [links, setLinks] = useState([]);
+  const [activeNode, setActiveNode] = useState(null);
 
-  useEffect(() => {
+ // ... (previous code)
+
+useEffect(() => {
+  const width = 1000;
+  const height = 800;
+
+  d3.json(
+    'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json'
+  ).then((data) => {
+    const initialNodes = data.nodes.map((node) => ({
+      ...node,
+      x: width / 2, // Set initial x position to the center
+      y: height / 2, // Set initial y position to the center
+    }));
+
+    setNodes(initialNodes);
+    setLinks(data.links);
+
+    const svg = d3.select(containerRef.current).append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .style('background-color', '#333');
+
+    const forceSim = d3
+      .forceSimulation(initialNodes) // Use initialNodes here
+      .force('link', d3.forceLink().links(links).strength(1.6))
+      .force('charge', d3.forceManyBody().strength(-0.75))
+      .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('collide', d3.forceCollide(20))
+      .on('tick', ticked);
+
+    // Rest of the code remains unchanged
+  });
+}, [ticked]);
+
+// ... (rest of the code)
+
+
     const width = 1000;
     const height = 800;
     d3.json(
@@ -14,7 +54,6 @@ function App() {
 
       const links = data.links;
       const nodes = data.nodes;
-
       const labels = d3.select('.labels');
       const container = d3.select(containerRef.current);
 
@@ -112,47 +151,70 @@ function App() {
 
 
 
-      function ticked() {
-          link
-            .attr('x1', (d) => d.source.x)
-            .attr('y1', (d) => d.source.y)
-            .attr('x2', (d) => d.target.x)
-            .attr('y2', (d) => d.target.y);
-          node
-            .style('left', (d) => d.x - 16 + 'px')
-            .style('top', (d) => d.y - 16 + 'px');
-        }
+     // ... (previous code)
+
+    function ticked() {
+  link
+    .attr('x1', (d) => d.source.x)
+    .attr('y1', (d) => d.source.y)
+    .attr('x2', (d) => d.target.x)
+    .attr('y2', (d) => d.target.y);
+  node.style('transform', (d) => `translate(${d.x}px, ${d.y}px)`);
+}
+
+// ... (rest of the code)
+
       
-      function dragstarted() {
-         
+        function dragstarted(event, d) {
+          if (!event.active) {
+            forceSim.alphaTarget(0.05).restart();
+          }
+          d.fx = d.x;
+          d.fy = d.y;
         }
-
-      function dragged(){
-
-      }
-
-      function dragended(){
-
-      }
+        
+        function dragged(event, d) {
+          d.fx = event.x;
+          d.fy = event.y;
+        }
+        
+        function dragended(event, d) {
+          if (!event.active) {
+            forceSim.alphaTarget(0);
+          }
+          d.fx = null;
+          d.fy = null;
+        }
 
     })
-  }, [])
+
+    const handleMouseOver = (node) => {
+      setActiveNode(node);
+    };
+  
+    const handleMouseOut = () => {
+      setActiveNode(null);
+    };
+
+    return (
+      <div ref={containerRef} className="container">
+        <div className="flags">
+          {nodes.map((node) => (
+            <img
+              key={node.code}
+              className={`flag flag-${node.code}`}
+              style={{ transform: 'scale(0.5)' }}
+              onMouseOver={() => handleMouseOver(node)}
+              onMouseOut={handleMouseOut}
+              draggable={false}
+            />
+          ))}
+        </div>
+      </div>
+    );
 
 
-
-
-
-
-
-
-  return <div ref={containerRef} className="container" />
      
-  
-  
-
-
-
-
 
 }
 export default App;
